28dd982d29972f5ff325e494d8909a4d5e904753
Naveena AM <35447452+naveenalur@users.noreply.github.com>
2024-12-15 10:45:35 +0530

    Add files via upload

---
+++ ClassAndObject/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace data_type_and_methods
+{
+    public class Employee
+    {
+        #region Properties
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        #endregion
+
+        #region Methods
+        public void Display()
+        {
+            Console.WriteLine("Employee Id: " + EmpId);
+            Console.WriteLine("Employee Name: " + Name);
+            Console.WriteLine("Employee City: " + City);
+        } 
+        #endregion
+    }
+}
---
+++ ClassAndObject/Program.cs
@@ -0,0 +1,31 @@
+﻿using data_type_and_methods;
+using System.Diagnostics.CodeAnalysis;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Employee empNaveen = new Employee();
+
+        Employee empTeju = new Employee();
+
+        Employee empBhima = new Employee();
+
+        empNaveen.EmpId = 100;
+        empNaveen.Name = "Naveen";
+        empNaveen.City = "Davanagere";
+        empNaveen.Display();
+        Console.WriteLine(empNaveen.GetHashCode());
+
+        Console.WriteLine("Employee Teju");
+        empTeju.Display();
+        Console.WriteLine(empTeju.GetHashCode());
+
+        Console.WriteLine("Employee Bhima");
+        empTeju.Display();
+        Console.WriteLine(empBhima.GetHashCode());
+
+    }
+
+}
